113f11fb5ddf9bc21d1d59e582c528f5bc5ce652
Alaa Eddine Ourmassi <aeourmassi@gmail.com>
2024-03-27 01:36:27 +0100

    add notifications dropdown menu

--- source-code/react-js/src/NotificationBell/NotificationBell.jsx
+++ source-code/react-js/src/NotificationBell/NotificationBell.jsx
@@ -1,18 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import "./NotificationBell.css";
+import { useState } from "react";
+import NotificationMenu from "./NotificationMenu";
 // import { useState } from "react";
 function NotificationBell() {
-  // const [showNotifications, setShowNotifications] = useState(false);
-  // const [notifications, setNotifications] = useState(["dasda"]);
+  const [showNotifications, setShowNotifications] = useState(false);
+  const [notifications, setNotifications] = useState(["dasda","this is Berlin here"]);
 
-  // const toggleNotifications = () => {
-  //   setShowNotifications((prevState) => !prevState);
-  // };
+  const toggleNotifications = () => {
+    setShowNotifications((prevState) => !prevState);
+  };
   return (
     <div className="notifier red">
-      <FontAwesomeIcon className="bell" icon={faBell} />
+      <FontAwesomeIcon className="bell" onClick={toggleNotifications} icon={faBell} />
       <span className="badge">5</span>
+      {showNotifications && (
+        <NotificationMenu notifications={notifications} />
+      )}
     </div>
   );
 }
---
+++ source-code/react-js/src/NotificationBell/NotificationMenu.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import "./NotificationMenu.css";
+
+const NotificationMenu = ({ notifications }) => {
+  return (
+    <div className="notification-menu">
+      <div className="notifications-header">
+       <strong>Notifications</strong>
+        <a href="">See All</a>
+      </div>
+      {notifications.length > 0 ? (
+        notifications.map((notification, index) => (
+          <div key={index} className="notification-item">
+            <div className="notification-item-info">
+              <div className="title"><p>Delivery successful to Tool</p></div>
+              <div className="description"><p>Order #9534 has been placed.</p></div>
+            </div>
+            <span className="duration">5 min</span>
+          </div>
+        ))
+      ) : (
+        <div className="no-notification">No new notifications</div>
+      )}
+      <div className="all-notifications">
+        <a>All Notifications</a>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationMenu;
