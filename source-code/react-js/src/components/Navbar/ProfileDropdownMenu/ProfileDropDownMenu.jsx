735b9aa8becb9c5cb5974e301e6392b7142f7664
Alaa Eddine Ourmassi <aeourmassi@gmail.com>
2024-03-29 23:58:35 +0100

    Close profile drop down menu when click out it

--- source-code/react-js/src/components/Navbar/ProfileDropdownMenu/ProfileDropDownMenu.jsx
+++ source-code/react-js/src/components/Navbar/ProfileDropdownMenu/ProfileDropDownMenu.jsx
@@ -1,20 +1,49 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import logoutSvg from "../../../assets/logout-svgrepo-com-black.svg";
 import userAvatar from "/kilter.jpg";
-import "./ProfileDropDownMenu.css";
+import styles from "./ProfileDropDownMenu.module.css"; // Import CSS module
+
 function ProfileDropDownMenu() {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const toggleButtonRef = useRef(null);
+  let profileDropdownMenuStyles = styles.profileDropdownMenu;
+  if (isOpen) {
+    profileDropdownMenuStyles += ` ${styles.open}`;
+  } else {
+    profileDropdownMenuStyles = styles.profileDropdownMenu;
+  }
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        toggleButtonRef.current !== event.target
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen]);
 
   const toggleMenu = () => {
     setIsOpen((prevState) => !prevState);
   };
 
   return (
-    <div className="dropdown-menu">
+    <div className={styles.dropdownMenu}>
       <FontAwesomeIcon
-        className="angle-down"
+        ref={toggleButtonRef}
+        className={styles.angleDown}
         icon={faAngleDown}
         onClick={toggleMenu}
         style={{
@@ -22,44 +51,44 @@ function ProfileDropDownMenu() {
           transition: "transform 0.3s ease",
         }}
       />
-      {isOpen && (
-        <div className="profile-dropdown-menu">
-          <div className="profile-details">
-            <img
-              src={userAvatar}
-              className="user-avatar-dropdown-menu"
-              alt="user avatar"
-            />
-            <div className="profile-name-email">
-              <p>Annette Black</p>
-              <span>annette@example.com</span>
-            </div>
-          </div>
-          <hr />
-          <ul>
-            <li>
-              <img src={logoutSvg} alt="" />
-              <p>Sign out</p>
-            </li>
-            <li>
-              <img src={logoutSvg} alt="" />
-              <p>Sign out</p>
-            </li>
-            <li>
-              <img src={logoutSvg} alt="" />
-              <p>Sign out</p>
-            </li>
-          </ul>
-
-          <div className="logout">
-            <hr />
-            <div className="logout-wrapper">
-              <img src={logoutSvg} alt="" />
-              <p>Sign out</p>
-            </div>
+      <div ref={dropdownRef} className={profileDropdownMenuStyles}>
+        <div className={styles.profileDetails}>
+          <img
+            src={userAvatar}
+            className={styles.userAvatarDropdownMenu}
+            alt="user avatar"
+          />
+          <div className={styles.profileNameEmail}>
+            <p>Annette Black</p>
+            <span className={styles.profileNameEmailSpan}>
+              annette@example.com
+            </span>
           </div>
         </div>
-      )}
+        <hr className={styles.profileDropdownMenuHr} />
+        <ul>
+          <li className={styles.profileDropdownMenuUlLi}>
+            <img src={logoutSvg} className={styles.menuIcon} alt="" />
+            <p>Sign out</p>
+          </li>
+          <li className={styles.profileDropdownMenuUlLi}>
+            <img src={logoutSvg} className={styles.menuIcon} alt="" />
+            <p>Sign out</p>
+          </li>
+          <li className={styles.profileDropdownMenuUlLi}>
+            <img src={logoutSvg} className={styles.menuIcon} alt="" />
+            <p>Sign out</p>
+          </li>
+        </ul>
+
+        <div className={styles.logout}>
+          <hr className={styles.logoutHr} />
+          <div className={styles.logoutWrapper}>
+            <img src={logoutSvg} className={styles.menuIcon} alt="" />
+            <p>Sign out</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
